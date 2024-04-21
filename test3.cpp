dc8cafdc1ebf92df51429965b9ae57abf261c7a3
Litus <109820050+Litusuwu@users.noreply.github.com>
2024-04-21 11:58:08 -0500

    dxd

---
+++ test3.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Daysi ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i < n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+#define pb push_back
+#define fill(x,v) memset(x,v,sizeof(x))
+#define all(v) (v).begin(),(v).end()
+#define F first
+#define S second
+
+typedef long double ld;
+typedef long long ll;
+typedef long long int li;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+ll genAns = 0;
+const int SN = 1e6;
+const int N = 3e3;
+bool prime[SN + 1];
+bool done[N][N];
+int dp[N][N];
+ 
+void SieveOfEratosthenes(int n); 
+
+void Solve(){
+    int n, k;
+    cin >> n >> k;
+    vl result;
+    ll sum = 1;
+    result.pb(1); 
+    n--; 
+    while (n > 1 && sum + (result.back() << 1) <= k - n +1) {
+        result.pb(result.back() << 1);
+        sum += result.back();
+        n--;
+    }
+        
+
+    while (n > 1) {
+       result.pb(1);
+        sum += 1;
+        n--;
+    }
+
+
+    result.pb(k - sum);
+        
+
+    for(ll num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+}
+
+
+int main(){
+    Daysi;
+    //SieveOfEratosthenes(SN);
+    int time = 1 ;
+    cin >> time ;
+    while(time--)Solve();
+    return 0;
+}
+
+void SieveOfEratosthenes(int n) {  
+    memset(prime, true, sizeof(prime)); 
+    for (int p = 2; p * p <= n; p++) { 
+        if (prime[p] == true) { 
+            for (int i = p * p; i <= n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+}
