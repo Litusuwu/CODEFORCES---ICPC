09d6184e1911ea5bd4a2139338d6c153cd6f13af
Litus <109820050+Litusuwu@users.noreply.github.com>
2024-04-21 08:58:08 -0500

    ostras

---
+++ A_Yogurt_Sale.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Daysi ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i < n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+#define pb push_back
+#define fill(x,v) memset(x,v,sizeof(x))
+#define all(v) (v).begin(),(v).end()
+#define F first
+#define S second
+
+typedef long double ld;
+typedef long long ll;
+typedef long long int li;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+ll genAns = 0;
+const int SN = 1e6;
+const int N = 3e3;
+bool prime[SN + 1];
+bool done[N][N];
+int dp[N][N];
+ 
+void SieveOfEratosthenes(int n); 
+
+void Solve(){
+    int n, m, k, l, d, r, ans = 0;
+    cin >> n >> m >> k;
+    if(k>(2*m)){
+        cout<<(n*m);
+    }
+    else{
+        if(n&1){
+            cout<<(k*((n-1)/2)+m);
+        }
+        else{
+            cout<<(k*(n/2));
+        }
+    }
+    cout << endl;
+}
+
+
+int main(){
+    Daysi;
+    //SieveOfEratosthenes(SN);
+    int time = 1 ;
+    cin >> time ;
+    while(time--)Solve();
+    return 0;
+}
+
+void SieveOfEratosthenes(int n) {  
+    memset(prime, true, sizeof(prime)); 
+    for (int p = 2; p * p <= n; p++) { 
+        if (prime[p] == true) { 
+            for (int i = p * p; i <= n; i += p) 
+                prime[i] = false; 
+        } 
+    } 
+}
