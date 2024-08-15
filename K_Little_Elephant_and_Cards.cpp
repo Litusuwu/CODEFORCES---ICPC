0c58d6f3886febb127a5fae897476fc15301288f
litusuwu <carlos.zegarrab@pucp.edu.pe>
2024-08-15 14:00:26 -0500

    lllll

--- K_Little_Elephant_and_Cards.cpp
+++ K_Little_Elephant_and_Cards.cpp
@@ -1,34 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
-#define rep(i,n) for (int i = 0; i < n; ++i)
-#define REP(i,k,n) for (int i = k; i < n; ++i)
-#define REPR(i,k,n) for (int i = k; i >= n; --i)
-#define pb push_back
-#define fill(x,v) memset(x,v,sizeof(x))
-#define all(v) (v).begin(),(v).end()
-#define F first
-#define S second
-#define endl "\n"
-typedef long double ld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<'['<< #x << "]: " << x << '\n'
+#else
+#endif
+
 typedef long long ll;
-typedef long long int li;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
+typedef long double ld;
+typedef pair<ll,ll> par;
 
-void Solve(){
-    int n, m, k, l, d, r, ans = 0;
-    
-}
+const ll N=2048;
+const ll MOD = 998244353LL;
+const ll INF = 1e18;
+const ld PI = acos(-1);
+const ld EPS = 1e-9;
 
+const char ZERO = '0';
+const string YES = "YES";
+const string NO = "NO";
 
-int main(){
-    fastio;
-    int time = 1 ;
-    //cin >> time ;
-    while(time--)Solve();
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    multiset<int>c1, c2;
+    set<int>c3;
+    for(int i = 0; i < n; i++){
+        int x, y;
+        cin >> x >> y;
+        if(x == y){
+            c1.insert(x);
+            c3.insert(x);
+            continue;
+        }
+        c3.insert(x);c3.insert(y);
+        c1.insert(x);c2.insert(y);
+    }
+
+    int ans =  INT_MAX, mini = n/2 + (n%2==0? 0 : 1);
+
+    for(auto it : c3){
+        // cout << it << " ";
+        // debug(it);
+        if(c1.count(it) >= mini){
+            ans = 0;
+            break;
+        }
+        int resp = c1.count(it) + c2.count(it);
+        if(resp >= mini){
+            ans = min(mini-(int)c1.count(it), ans);
+        }
+    }
+    cout <<(ans==INT_MAX? -1 :ans) << endl;
     return 0;
 }
-
